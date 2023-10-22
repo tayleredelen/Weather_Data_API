ef3c74c64686f87f2bb7645ab78f732c1ad4b3f3
tayleredelen <taylerledelen@gmail.com>
2023-10-21 19:15:57 -0500

    Day 29 -- updated app routes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 # website object instance
-app = Flask("Website")
+app = Flask(__name__)
 
 
 # flask is configured to look for "templates" folder in root path
@@ -10,14 +10,19 @@ app = Flask("Website")
 # to view tutorial.html with this code you have to add /home to the url path, this creates an error
 # alternatively could delete the param in @app.route to show the tutorial.html file
 # the @ symbol on app is a decorator, connecting decorator (url path) to the function
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>/")
+def about(station, date):
+    # df = pandas.read.csv("")
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
