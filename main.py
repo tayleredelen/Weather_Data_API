6b0f2b5464ddf14701ea3c3cb2f4819bec13f170
tayleredelen <taylerledelen@gmail.com>
2023-10-24 16:47:08 -0500

    Day 30 -- working on small_data file

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ def home():
 
 @app.route("/api/v1/<station>/<date>/")
 def about(station, date):
-    df = pd.read_csv("data_small/TG_STAID000001.txt")
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # skiprows skips the first 20 rows of metadata
+    temperature = df.loc[df['    DATE'] == date]['    TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
