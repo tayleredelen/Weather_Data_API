eff07ea7e854f7a9bbb06f0c2140220cc45cc987
tayleredelen <taylerledelen@gmail.com>
2023-10-25 13:03:31 -0500

    Day 30 -- return temperature for any station and date

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-import pandas
 from flask import Flask, render_template
 import pandas as pd
 
 # website object instance
 app = Flask(__name__)
-
-df = pandas.read.csv("/data_small")
-
-
 # flask is configured to look for "templates" folder in root path
 
 # when user visits the /home url the home() function is called
 # to view tutorial.html with this code you have to add /home to the url path, this creates an error
 # alternatively could delete the param in @app.route to show the tutorial.html file
 # the @ symbol on app is a decorator, connecting decorator (url path) to the function
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<station>/<date>/")
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     # skiprows skips the first 20 rows of metadata
-    temperature = df.loc[df['    DATE'] == date]['    TG'].squeeze() / 10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
