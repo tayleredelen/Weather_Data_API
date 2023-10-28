c78621a8f9cff3e22405b9b3a2851c6044e798af
tayleredelen <taylerledelen@gmail.com>
2023-10-28 10:00:07 -0500

    Day 30 -- add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -30,6 +30,23 @@ def about(station, date):
             "temperature": temperature}
 
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
 #     default port is 5000, giving this argument allows multiple apps to run at once
